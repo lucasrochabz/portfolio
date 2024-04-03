916b3d98328aadd6aabb2b98cbb4bdf9f69842ed
Lucas Rocha <lucasbezerrar@gmail.com>
2024-04-03 15:58:50 -0300

    Colocando '.' no src do js

--- js/modules/modal.js
+++ js/modules/modal.js
@@ -9,7 +9,6 @@ export default function initModal() {
     
     lightboxImage.setAttribute('src', imageUrl)
     lightbox.style.display = 'flex'
-
   }
 
   fotoPerfil.addEventListener('click', ampliarFoto);
