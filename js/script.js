1e12c86eabeac3e0b214c26518ad5a38abfad8c7
Lucas Rocha <lucasbezerrar@gmail.com>
2024-04-03 23:55:40 -0300

    Resolvendo problema de srcoll lateral

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 import initModal from "./modules/modal.js";
 import initScrollSuave from "./modules/scroll-suave.js";
+import initMenuMobile from "./modules/menu-mobile.js";
 
 initModal();
 initScrollSuave();
+initMenuMobile();
