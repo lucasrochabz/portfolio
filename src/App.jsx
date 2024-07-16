fa8f6077658876ea755d66b35e8cc52c0e44c3be
lucasrochabz <lucasbezerrar@gmail.com>
2024-07-15 21:08:06 -0300

    Add component footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css';
+import { Footer } from './components/Footer/Footer';
 import { Header } from './components/Header/Header';
 
 export const App = () => {
   return (
     <>
       <Header />
-      <h2>Hello World!</h2>;
+      <h2>Hello World!</h2>
+      <Footer />
     </>
   );
 };
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import './Footer.css';
+
+export const Footer = () => {
+  return (
+    <>
+      <footer className="footer">
+        <p>&copy; 2024 Lucas Rocha. Todos os direitos reservados.</p>
+      </footer>
+    </>
+  );
+};
