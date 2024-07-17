0e69c7e542d3742339de8c738961c85407f9e4a9
lucasrochabz <lucasbezerrar@gmail.com>
2024-07-17 20:49:43 -0300

    Add component contact

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import './App.css';
-import { Footer } from './components/Footer/Footer';
-import { About } from './components/Header/About/About';
 import { Header } from './components/Header/Header';
+import { About } from './components/About/About';
 import { ProjectList } from './components/ProjectList/ProjectList';
+import { Footer } from './components/Footer/Footer';
+import { Contact } from './components/Contact/Contact';
 
 export const App = () => {
   return (
@@ -10,6 +11,7 @@ export const App = () => {
       <Header />
       <About />
       <ProjectList />
+      <Contact />
       <Footer />
     </>
   );
---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,42 @@
+import './Contact.css';
+
+export const Contact = () => {
+  const handleBtnMessage = (event) => {
+    event.preventDefault();
+    console.log('clicou');
+  };
+
+  return (
+    <>
+      <div className="contact-container">
+        <form className="form-contact">
+          <div className="form-top">
+            <div className="input-top">
+              <label htmlFor="name">Nome</label>
+              <input type="text" id="name" placeholder="Seu nome" />
+            </div>
+
+            <div className="input-top">
+              <label htmlFor="phone">Telefone</label>
+              <input type="text" id="phone" placeholder="(85) 99999-9999" />
+            </div>
+          </div>
+
+          <label htmlFor="email">Email</label>
+          <input type="text" id="email" placeholder="contato@email.com" />
+
+          <label htmlFor="message">Mensagem</label>
+          <textarea
+            name="message"
+            id="message"
+            placeholder="O que você precisa?"
+          ></textarea>
+
+          <button className="btn-send-message" onClick={handleBtnMessage}>
+            Enviar Mensagem
+          </button>
+        </form>
+      </div>
+    </>
+  );
+};
