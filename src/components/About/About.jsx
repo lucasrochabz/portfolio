521b6030414fbe6a863a86419085fc0848a58b19
lucasrochabz <lucasbezerrar@gmail.com>
2025-04-09 21:10:57 -0300

    add sticker

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,20 +1,24 @@
+import lucas from '../../../public/lucas01.png';
 import styles from './About.module.css';
 
 export const About = () => {
   return (
     <>
       <section className={styles.about} id="about">
-        <p>- Olá, eu sou o Lucas Rocha 👋</p>
-        <h2>Desenvolvedor Web</h2>
-        <h2>Full Stack</h2>
-        <span>
-          Olá e bem vindos. Desenvolvedor Full Stack criativo de Fortaleza
-          apaixonado por criar experiências memoráveis.
-        </span>
-        <span>
-          Olá e bem vindos. Desenvolvedor Full Stack criativo de Fortaleza
-          apaixonado por criar experiências memoráveis.
-        </span>
+        <figure className={styles.sticker}>
+          <img src={lucas} alt="Lucas" />
+        </figure>
+        <div className={styles.content}>
+          <p>- Olá, eu sou o Lucas Rocha 👋</p>
+          <h2>Desenvolvedor Web</h2>
+          <h2>Full Stack</h2>
+          <p>
+            Olá e bem vindos. Desenvolvedor Full Stack criativo de Fortaleza
+            apaixonado por criar experiências memoráveis. Olá e bem vindos.
+            Desenvolvedor Full Stack criativo de Fortaleza apaixonado por criar
+            experiências memoráveis.
+          </p>
+        </div>
       </section>
     </>
   );
