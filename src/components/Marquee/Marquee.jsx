27de8101ccb10da3fd0b5a099cb8d69f98a50c34
lucasrochabz <lucasbezerrar@gmail.com>
2026-07-29 23:49:22 -0300

    add novas ferramentas a lista de tools

--- src/components/Marquee/Marquee.jsx
+++ src/components/Marquee/Marquee.jsx
@@ -2,7 +2,7 @@ import { profile } from '@/data/profile';
 import styles from './Marquee.module.css';
 
 const Marquee = () => {
-  const repeatedStacks = profile.skills.concat(profile.skills);
+  const repeatedStacks = profile.tools.concat(profile.tools);
 
   return (
     <section className={styles.marquee}>
