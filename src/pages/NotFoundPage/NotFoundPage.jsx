9065bdc7445ae0c4583a9eecbc3ac0b952d2fe36
lucasrochabz <lucasbezerrar@gmail.com>
2026-09-09 22:31:41 -0300

    add illustrations

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,15 +1,22 @@
 import { PATHS } from '@/constants/paths';
 import { Heading } from '@/components/Heading';
 import { InternalLink } from '@/components/InternalLink';
+import warning from '@/assets/illustrations/warning.svg';
+import styles from './NotFoundPage.module.css';
 
+// fix: corrigir essa página
 const NotFoundPage = () => {
   return (
-    <>
-      <Heading>Página não encontrada</Heading>
-      <InternalLink to={PATHS.HOME} variant={'fill'}>
-        Voltar para a página inicial
-      </InternalLink>
-    </>
+    <section className={styles.notFound}>
+      <img src={warning} alt="Aviso" />
+
+      <div>
+        <Heading>Página não encontrada</Heading>
+        <InternalLink to={PATHS.HOME} variant={'fill'}>
+          Voltar à página inicial
+        </InternalLink>
+      </div>
+    </section>
   );
 };
 
